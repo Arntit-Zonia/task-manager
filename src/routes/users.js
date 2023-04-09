623f287a39f04ac5246610b414375abd3d3a051d
Arntit Zonia <arditzonia@gmail.com>
2023-04-09 18:43:52 +0100

    Refactored user routes to use the auth middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const router = new express.Router();
 const errorHandler = require("../middleware/errorHandler");
+const auth = require("../middleware/auth");
 
 const User = require("../models/users");
 
@@ -9,63 +10,89 @@ const User = require("../models/users");
  * @todo fix prettier formatting
  */
 router.get(
-  "/users/:id",
+  "/users/me",
+  auth,
   errorHandler(async (req, res) => {
-    const getUser = await User.findById(req.params.id);
-
-    if (!getUser) return res.status(404).send("User not found");
-
-    return res.status(200).send(getUser);
-  })
-);
-
-router.get(
-  "/users",
-  errorHandler(async (_req, res) => {
-    const getUsers = await User.find({});
-
-    return res.status(200).send(getUsers);
+    res.status(200).send(req.user);
   })
 );
 
 router.patch(
-  "/users/:id",
-  errorHandler(async (req, res) => {
+  "/users/me",
+  auth,
+  errorHandler(async ({ user, body }, res) => {
     const allowedUpdates = ["name", "email", "password", "age"];
-    const updates = Object.keys(req.body);
+    const updates = Object.keys(body);
     const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
 
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!user) return res.status(404).send("User not found");
     if (!isValidUpdate) return res.status(400).send("Invalid update");
+
+    updates.forEach((update) => (user[update] = body[update]));
+
+    await user.save();
 
     res.status(200).send(user);
   })
 );
 
+// add user
 router.post(
   "/users",
   errorHandler(async (req, res) => {
     const user = new User(req.body);
+    const token = await user.generateAuthToken();
 
     await user.save();
 
-    res.status(201).send(user);
+    res.status(201).send({ user, token });
+  })
+);
+
+router.post(
+  "/users/login",
+  errorHandler(async (req, res) => {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+
+    if (!user) return res.status(404).send("User not found");
+
+    res.status(200).send({ user, token });
+  })
+);
+
+router.post(
+  "/users/logout",
+  auth,
+  errorHandler(async ({ user, token: currentToken }, res) => {
+    user.tokens = user.tokens.filter((token) => token.token !== currentToken);
+
+    await user.save();
+
+    res.status(200).send({ tokens: user.tokens, removedToken: currentToken });
+  })
+);
+
+router.post(
+  "/users/logoutAll",
+  auth,
+  errorHandler(async ({ user }, res) => {
+    const { tokens } = user;
+
+    user.tokens = [];
+
+    await user.save();
+
+    res.status(200).send({ user, removedTokens: tokens });
   })
 );
 
 router.delete(
-  "/users/:id",
-  errorHandler(async (req, res) => {
-    const getUser = await User.findByIdAndDelete(req.params.id);
+  "/users/me",
+  auth,
+  errorHandler(async ({ user }, res) => {
+    await user.deleteOne();
 
-    if (!getUser) return res.status(404).send("User not found");
-
-    return res.status(200).send(getUser);
+    return res.status(200).send(user);
   })
 );
 
