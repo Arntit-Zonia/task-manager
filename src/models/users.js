e492cbde03178ab3d29365a38a2e0ae274e57b77
Arntit Zonia <arditzonia@gmail.com>
2023-04-09 18:42:49 +0100

    Added additional user schema middleware

--- src/models/users.js
+++ src/models/users.js
@@ -1,24 +1,27 @@
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const { Schema, model } = require("mongoose");
+
+const Task = require("./tasks");
 
 const userSchema = new Schema({
   name: { type: String, required: true, trim: true },
   occupation: {
     type: String,
     validate(value) {
-      if (value === "Receptionist")
-        throw new Error("We don't do that anymore!");
+      if (value === "Receptionist") throw new Error("We don't do that anymore!");
     },
   },
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
-      if (!validator.isEmail(value))
-        throw new Error("Please insert a valid Email address!");
+      if (!validator.isEmail(value)) throw new Error("Please insert a valid Email address!");
     },
   },
   password: {
@@ -27,10 +30,77 @@ const userSchema = new Schema({
     trim: true,
     minLength: 7,
     validate(value) {
-      if (value.toLowerCase().includes("password"))
-        throw new Error('Password cannot contain "password"');
+      if (value.toLowerCase().includes("password")) throw new Error('Password cannot contain "password"');
     },
   },
+  tokens: [
+    {
+      token: { type: String, required: true },
+    },
+  ],
+});
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+
+  const token = jwt.sign({ _id: user._id.toString() }, "testsecret");
+
+  user.tokens.push({ token });
+
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) throw new Error("Email not found");
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) throw new Error("Password does not match");
+
+  return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    const hashedPassword = await bcrypt.hash(user.password, 8);
+
+    user.password = hashedPassword;
+  }
+
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre("deleteOne", { document: true, query: false }, async function (next) {
+  const user = this;
+
+  await Task.deleteMany({ owner: user._id });
+
+  next();
 });
 
 const User = model("User", userSchema);
