1b6720542c6d5851207b41aee154e58c9648407a
Arntit Zonia <arditzonia@gmail.com>
2023-04-11 19:01:55 +0100

    updated route error handler file name

--- src/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-/**
- * Middleware function that wraps route handlers to catch and handle errors.
- * @param {Function} routeHandler - Function that is wrapped with the error handling.
- * @returns {Function} Middleware function with error handling.
- */
-const errorHandler = (routeHandler) => (req, res, next) =>
-  Promise.resolve(routeHandler(req, res, next)).catch(next);
-
-module.exports = errorHandler;
