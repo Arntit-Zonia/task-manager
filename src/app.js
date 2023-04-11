8acaf32d4ee29238fd87d73a305bbe1c4c04c912
Arntit Zonia <arditzonia@gmail.com>
2023-04-11 19:07:32 +0100

    added middleware error handler to app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const usersRouter = require("./routes/users");
 const tasksRouter = require("./routes/tasks");
 
 const initMongoose = require("./db/mongoose");
+const middlewareErrorHandler = require("./middleware/middlewareErrorHandler");
 
 const app = express();
 const port = 3000;
@@ -15,14 +16,6 @@ app.use(express.json());
 app.use(usersRouter);
 app.use(tasksRouter);
 
-/**
- * error handler middleware
- * @todo refactor implementation
- */
-app.use((err, _req, res, _next) => {
-  console.error({ err });
-
-  res.status(err.status || 500).send({ error: err.message });
-});
+app.use(middlewareErrorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
