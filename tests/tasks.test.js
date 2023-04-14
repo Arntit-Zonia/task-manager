3df336606c68f9b4ae6beb3414811309d59d56f9
Arntit Zonia <arditzonia@gmail.com>
2023-04-14 23:02:48 +0100

    Created Tasks unit test structure

---
+++ tests/tasks.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+
+const app = require("../src/app");
+const { mockObjectId, mockUser, setUpDatabase } = require("./fixtures/db");
+
+const Task = require("../src/models/tasks");
+
+describe("Tasks", () => {
+  describe("GET", () => {});
+
+  describe("POST", () => {});
+
+  describe("PATCH", () => {});
+
+  describe("DELETE", () => {});
+});
