b9b1285f9bbf0fb361db7a61c7fd7aac0262dfde
Arntit Zonia <arditzonia@gmail.com>
2023-04-15 18:52:27 +0100

    Created Tasks mock values

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -2,28 +2,53 @@ const { ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 
 const User = require("../../src/models/users");
+const Task = require("../../src/models/tasks");
 
-const mockObjectId = new ObjectId();
+const mockUserId = new ObjectId();
 const mockUser = {
-  _id: mockObjectId,
+  _id: mockUserId,
   name: "first-user",
   password: "123456789",
   email: "first@random.com",
   avatar: Buffer.from("random-avatar"),
   tokens: [
     {
-      token: jwt.sign({ _id: mockObjectId }, process.env.JWT_SECRET),
+      token: jwt.sign({ _id: mockUserId }, process.env.JWT_SECRET),
     },
   ],
 };
 
+const mockTaskOne = {
+  _id: new ObjectId(),
+  description: "First test task",
+  completed: false,
+  owner: mockUserId,
+};
+
+const mockTaskTwo = {
+  _id: new ObjectId(),
+  description: "Second test task",
+  completed: true,
+  owner: mockUserId,
+};
+
+const bearerToken = `Bearer ${mockUser.tokens[0].token}`;
+
 const setUpDatabase = async () => {
   await User.deleteMany();
+  await Task.deleteMany();
+
   await new User(mockUser).save();
+
+  await new Task(mockTaskOne).save();
+  await new Task(mockTaskTwo).save();
 };
 
 module.exports = {
-  mockObjectId,
+  mockUserId,
   mockUser,
+  mockTaskOne,
+  mockTaskTwo,
+  bearerToken,
   setUpDatabase,
 };
